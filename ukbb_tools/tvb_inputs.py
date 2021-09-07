3a1744ed1d1bdb3093ad27e0b1774a798453c2e2
Wayne <yile.wang@utdallas.edu>
2021-09-07 17:47:24 -0500

    interval calculation

--- ukbb_tools/tvb_inputs.py
+++ ukbb_tools/tvb_inputs.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-f = open("C:/Users/Wayne/tvb/tvb_inputs/sc.txt", "r")
+#f = open("/home/wayne/tvb_inputs/sc.txt", "r")
+f = open("/home/wayne/tvb_inputs/distance.txt", "r")
 sc = np.loadtxt(f)
 ax = sns.heatmap(sc)
 ax.set_title('sc.txt')
