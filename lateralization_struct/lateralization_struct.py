2f9dca5cba65a365ba24ca1a2b55466c9182cb3b
Wayne <ylwwayne@gmail.com>
2021-11-04 09:16:39 -0500

    change path

--- lateralization_struct/lateralization_struct.py
+++ lateralization_struct/lateralization_struct.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     pcgSC = pd.DataFrame(columns=['grp', 'caseid', 'sc'])
     for grp in grp_pools:
         # obtain the data path
-        pth = 'C:/Users/Wayne/tvb/LFP/'+grp
+        pth = 'C:/Users/Wayne/tvb/AUS/'+grp
         case_pools = os.listdir(pth)
         # iterate the case id.
         # color = col[xx]
@@ -29,7 +29,7 @@ if __name__ == '__main__':
             pcgSC = pcgSC.append(tmp, ignore_index=True)
     fig = plt.figure(figsize=(10,10))
     plt.title('Homotopic-Streamlines')
-    fig = sns.violinplot(x="grp", y="sc", data=pcgSC, capsize=.2,palette=["#66CDAA","#4682B4","#AB63FA","#FFA15A"])
+    fig = sns.violinplot(x="grp", y="sc", data=pcgSC, capsize= .2,palette=["#66CDAA","#4682B4","#AB63FA","#FFA15A"])
     fig = sns.stripplot(x="grp", y="sc", data=pcgSC,color='black')
     fig = sns.pointplot(data=pcgSC, x='grp', y="sc", join=False, ci=None, color='red')
     # fig.set_ylim(-0.5, 1)
