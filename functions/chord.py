f528f3b7c09ce7c7c502a7a42606a6795ac31c15
Wayne <ylwwayne@gmail.com>
2022-01-14 11:46:58 -0600

    update the chord plot file

--- functions/chord.py
+++ functions/chord.py
@@ -4,24 +4,41 @@ from mne_connectivity.viz import circular_layout, plot_connectivity_circle
 import numpy as np
 import pandas as pd
 import io
+import os
+import matplotlib.pyplot as plt
 
 N = 8
 node_names = ['aCNG-L', 'aCNG-R','mCNG-L','mCNG-R','pCNG-L','pCNG-R', 'HIP-L','HIP-R','PHG-L','PHG-R','AMY-L','AMY-R', 'sTEMp-L','sTEMp-R','mTEMp-L','mTEMp-R']
 split_codes = ['aCNG-L', 'mCNG-L', 'pCNG-L', 'HIP-L', 'PHG-L', 'AMY-L', 'sTEMp-L','mTEMp-L', 'aCNG-R', 'mCNG-R', 'pCNG-R', 'HIP-R', 'PHG-R', 'AMY-R', 'sTEMp-R','mTEMp-R']
 
-datapath = r'C:\Users\Wayne\tvb\AUS\SNC\8709A\weights.txt'
-scl = open(datapath,"r")
-lines = scl.read()
-tmp_d = pd.read_csv(io.StringIO(lines), sep='\t', header=None, index_col=None, engine="python")
-tmp_df = tmp_d.set_axis(node_names, axis=0) # set the index
-df_sc = tmp_df.set_axis(node_names, axis=1) # set the columns
+grp_pools = ['SNC', 'NC','MCI', 'AD']
+mean_group_matrix = np.zeros((16, 16, 4))
+fig1 = plt.figure(figsize=(3,3))
+for indx, grp in enumerate(grp_pools):
+# obtain the data path
+    pth = 'C:/Users/Wayne/tvb/AUS/' + grp
+    case_pools = os.listdir(pth)
+    group_matrix = np.zeros((16, 16, len(case_pools)))
+    for ind, caseid in enumerate(case_pools):
+        ### the structural connectivity ###
+        # SC lateralization
+        # fig, (ax0, ax1) = plt.subplots(1,2, figsize=(15,5))
+        # fig.suptitle(grp+'_'+caseid)
+        datapath = 'C:/Users/Wayne/tvb/AUS/' + grp + '/' + caseid + '/weights.txt'
+        scl = open(datapath,"r")
+        lines = scl.read()
+        tmp_d = pd.read_csv(io.StringIO(lines), sep='\t', header=None, index_col=None, engine="python")
+        tmp_df = tmp_d.set_axis(node_names, axis=0) # set the index
+        df_sc = tmp_df.set_axis(node_names, axis=1) # set the columns
+        df_sc = df_sc.reindex(index=split_codes, columns=split_codes)
+        group_matrix[:,:,ind] = df_sc
+    node_order = list()
+    node_order.extend(split_codes[:8])  # reverse the order
+    node_order.extend(split_codes[8:][::-1])
+    node_angles = circular_layout(split_codes, node_order, start_pos=90,
+                                group_boundaries=[0, len(split_codes) / 2])
 
-df_sc = df_sc.reindex(index=split_codes, columns=split_codes)
+    fig, axis = plot_connectivity_circle(np.array(np.mean(group_matrix, 2)), split_codes, node_angles=node_angles,colormap='Reds', facecolor='white', textcolor='black', colorbar=False, linewidth=3, title=grp, fontsize_names=10, show=False, fig = fig1)
+    fig.savefig(f"{grp}_sc.png", dpi=300)
 
-node_order = list()
-node_order.extend(split_codes[:8])  # reverse the order
-node_order.extend(split_codes[8:][::-1])
-node_angles = circular_layout(split_codes, node_order, start_pos=90,
-                              group_boundaries=[0, len(split_codes) / 2])
 
-fig, axis = plot_connectivity_circle(np.array(df_sc), split_codes, node_angles=node_angles,colormap='Blues', facecolor='white', textcolor='black', colorbar=False, linewidth=10, title="Structural Connectivity in Limbic Subnetwork", fontsize_names=10)
