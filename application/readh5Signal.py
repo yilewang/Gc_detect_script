cd61640958dd585489753fbf7f2f00c7ffb1412d
Yile Wang <yxw190015@utdallas.edu>
2022-06-14 12:32:50 -0500

    update signal

--- application/readh5Signal.py
+++ application/readh5Signal.py
@@ -6,17 +6,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 sys.path.append("/mnt/c/Users/wayne/tvb/tvbtools")
-from tools.signal import fir_bandpass
+from tools.signal import fir_bandpass, hdf5Reader
 
 filename = "/mnt/c/Users/wayne/tvb/gc3mins/AD/0306A.h5"
-with h5py.File(filename, "r") as f:
-    ### get key of h5 file
-    # print(list(f.keys()))
-    time = np.arange(0, 180, 1/81920)
-    dset = f['raw']
-    pCNGRTheta, N, delay= fir_bandpass(dset[:,5], 81920., 2., 10.)
-    fig, ax = plt.subplots(figsize=(100,15))
-    ax.plot(time, dset[:, 5], label = "pCNGR-raw")
-    ax.plot(time[N-1:]-delay, pCNGRTheta[N-1:], label = "pCNGR-Filter")
-    ax.legend()
-    plt.show()
+time = np.arange(0, 180, 1/81920)
+
+dset = hdf5Reader(filename)
+pCNGRTheta, N, delay= fir_bandpass(dset[:,5], 81920., 2., 10.)
+fig, ax = plt.subplots(figsize=(100,15))
+ax.plot(time, dset[:, 5], label = "pCNGR-raw")
+ax.plot(time[N-1:]-delay, pCNGRTheta[N-1:], label = "pCNGR-Filter")
+ax.legend()
+plt.show()
--- tools/signal.py
+++ tools/signal.py
@@ -4,27 +4,38 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import signal
+import h5py
+
+def hdf5Reader(filename):
+    with h5py.File(filename, "r") as f:
+    ### get key of h5 file
+        key = list(f.keys())
+        dset = f[key[0]][:]
+        return dset
 
 
-
-
-def psd(signal, samplinginterval, fs):
+def psd(signal, samplinginterval, visual=False):
+    """
+    This function is for power spectrum density analysis
+    Input:
+        Signal, sampling interveral (1/fs), visual (default is False)
+    Output:
+        Freq axis, PSD of the signal
+    """
     fourierSignal = np.fft.fft(np.array(signal) - np.array(signal).mean())
-    spectrum = 2 * (1/fs) ** 2 / 1000 * (fourierSignal * fourierSignal.conj())
+    spectrum = 2 * (samplinginterval) ** 2 / 1000 * (fourierSignal * fourierSignal.conj())
     spectrum = spectrum[:int(len(np.array(signal)) / 2)] 
     time_all = 1 / 1
     fNQ = 1/samplinginterval/2 # Nyquist frequency
     faxis = np.arange(0, fNQ, time_all) # frequency axis
+    if visual:
+        fig, axs = plt.subplots()
+        psd_freq, psd_signal = psd(np.array(signal), samplinginterval)
+        axs.plot(psd_freq, psd_signal, color='r', label = 'PSD Results')
+        axs.legend()
+        plt.show()
     return faxis, spectrum.real
 
-def psd_plot(dfR, dfL, samplinginterval):
-    fig, axs = plt.subplots(2, figsize=(15,7))
-    psd_freq_right, psd_right = psd(np.array(dfR), samplinginterval)
-    psd_freq_left, psd_left = psd(np.array(dfL), samplinginterval)
-    axs[0].plot(psd_freq_left, psd_left, color= 'k', label = 'PCG Left')
-    axs[0].plot(psd_freq_right, psd_right, color='r', label = 'PCG Right')
-    axs[0].set(xlim=[0,100])
-    axs[0].legend()
 
 
 def fir_bandpass(data, fs, cut_off_low, cut_off_high, width=2.0, ripple_db=10.0):
@@ -48,3 +59,7 @@ def fir_bandpass(data, fs, cut_off_low, cut_off_high, width=2.0, ripple_db=10.0)
     filtered_signal = signal.lfilter(taps, 1.0, data)
     delay = 0.5 * (N-1) / fs
     return filtered_signal, N, delay
+
+
+def freqCount(signal, filter=False):
+    pCNGRTheta, N, delay= fir_bandpass(dset[:,5], 81920., 2., 10.)
