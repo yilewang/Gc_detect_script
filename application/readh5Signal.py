af577726f343d561489222c3ff95c91fed674bce
yile <ylwwayne@gmail.com>
2022-06-14 15:13:28 -0500

    update the signaltools module

--- application/readh5Signal.py
+++ application/readh5Signal.py
@@ -5,16 +5,19 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-sys.path.append("/mnt/c/Users/wayne/tvb/tvbtools")
-from tools.signal import fir_bandpass, hdf5Reader
+sys.path.append("/mnt/w/github/tvbtools")
+from tools.signalTools import freqCount, hdf5Reader
 
-filename = "/mnt/c/Users/wayne/tvb/gc3mins/AD/0306A.h5"
+filename = "/mnt/w/go3mins/SNC/2820A.h5"
 time = np.arange(0, 180, 1/81920)
 
 dset = hdf5Reader(filename)
-pCNGRTheta, N, delay= fir_bandpass(dset[:,5], 81920., 2., 10.)
-fig, ax = plt.subplots(figsize=(100,15))
-ax.plot(time, dset[:, 5], label = "pCNGR-raw")
-ax.plot(time[N-1:]-delay, pCNGRTheta[N-1:], label = "pCNGR-Filter")
-ax.legend()
-plt.show()
+
+freqNum = freqCount(dset[:,5], 0.5, 81920., filter = True, visual=True, dpi = 30)
+print(freqNum)
+# pCNGRTheta, N, delay= fir_bandpass(dset[:,5], 81920., 2., 10.)
+# fig, ax = plt.subplots(figsize=(100,15))
+# ax.plot(time, dset[:, 5], label = "pCNGR-raw")
+# ax.plot(time[N-1:]-delay, pCNGRTheta[N-1:], label = "pCNGR-Filter")
+# ax.legend()
+# plt.show()
---
+++ tools/signalTools.py
@@ -0,0 +1,99 @@
+#!/bin/usr/python
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import signal
+import h5py
+
+def hdf5Reader(filename):
+    with h5py.File(filename, "r") as f:
+    ### get key of h5 file
+        key = list(f.keys())
+        dset = f[key[0]][:]
+        return dset
+
+
+def psd(data, samplinginterval, visual=False, *args, **plt_kwargs):
+    """
+    This function is for power spectrum density analysis
+    Input:
+        Signal, sampling interveral (1/fs), visual (default is False)
+    Output:
+        Freq axis, PSD of the signal
+    """
+    fourierSignal = np.fft.fft(np.array(data) - np.array(data).mean())
+    spectrum = 2 * (samplinginterval) ** 2 / 1000 * (fourierSignal * fourierSignal.conj())
+    spectrum = spectrum[:int(len(np.array(data)) / 2)] 
+    time_all = 1 / 1
+    fNQ = 1/samplinginterval/2 # Nyquist frequency
+    faxis = np.arange(0, fNQ, time_all) # frequency axis
+    if visual:
+        fig, axs = plt.subplots(*args, **plt_kwargs)
+        psd_freq, psd_signal = psd(np.array(data), samplinginterval)
+        axs.plot(psd_freq, psd_signal, color='r', label = 'PSD Results', *args, **plt_kwargs)
+        axs.legend()
+        plt.show()
+    return faxis, spectrum.real
+
+
+
+def fir_bandpass(data, fs, cut_off_low, cut_off_high, width=2.0, ripple_db=10.0):
+    """
+    The FIR bandpass filter
+    Args:
+        data: 1-d array
+        fs: frequency (sampling rate)
+        cut_off_low: the low threshold
+        cut_off_high: the high threshold
+        width: the time windows for filtering
+    Return:
+        filtered data, N, delay (for plotting)
+        when plot, align the axis by `plt.plot(time[N-1:]-delay, filtered_data[N-1:])`
+    """
+    nyq_rate = fs / 2.0
+    wid = width/nyq_rate
+    N, beta = signal.kaiserord(ripple_db, wid)
+    taps = signal.firwin(N, cutoff = [cut_off_low, cut_off_high],
+                  window = 'hamming', pass_zero = False, fs=fs)
+    filtered_signal = signal.lfilter(taps, 1.0, data)
+    delay = 0.5 * (N-1) / fs
+    return filtered_signal, N, delay
+
+
+def freqCount(data, prominence, fs, filter=False, visual = False, length=10, height=5, dpi = 50, *args, **kwargs):
+    """
+    The *args argument in a function definition allows 
+    the function to process an unspecified number of positional arguments.
+    
+    You can use the *args notation to “unpack” a sequence 
+    into a series of positional arguments for any function. 
+
+    **kwargs: To do this, you must package the keyword-value pairs in a dictionary.
+    """
+    data = np.array(data)
+    time = np.arange(0, len(data)/fs, 1/fs)
+    spikesdata, _ = signal.find_peaks(data, prominence = prominence)
+
+    if filter:
+        postfilter, N, delay= fir_bandpass(data, fs, 2., 10.)
+        spikesfiltered, _ = signal.find_peaks(postfilter, prominence = prominence)
+    # visualization
+    if visual:
+        fig, ax = plt.subplots(figsize=(length, height), dpi = dpi)
+        ax.plot(time, data, label = "signal", *args, **kwargs)
+        ax.plot(spikesdata/fs, data[spikesdata], '+', label = "signal spikes")
+        if filter:
+            ax.plot(time[N-1:]-delay, postfilter[N-1:], label = "filtered signal")
+            if len(spikesfiltered) > 0:
+                ax.plot(spikesfiltered[spikesfiltered > N-1]/fs - delay, postfilter[spikesfiltered[spikesfiltered > N-1]],'x', label = "filtered spikes")
+        ax.legend()
+        plt.show()
+
+    # return output
+    if filter:
+        return (len(spikesdata), len(spikesfiltered))
+    else:
+        return len(spikesdata)
+    
+        
