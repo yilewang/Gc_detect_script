be1421a97fc53ed875c821167b8888823cf03c1e
Yile Wang <yxw190015@utdallas.edu>
2022-06-14 11:33:25 -0500

    update branch

--- application/readh5Signal.py
+++ application/readh5Signal.py
@@ -5,10 +5,10 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-sys.path.append("/mnt/w/github/tvbtools")
-from tools.FIR_filter import fir_bandpass
+sys.path.append("/mnt/c/Users/wayne/tvb/tvbtools")
+from tools.signal import fir_bandpass
 
-filename = "/mnt/w/go3mins/AD/0306A.h5"
+filename = "/mnt/c/Users/wayne/tvb/gc3mins/AD/0306A.h5"
 with h5py.File(filename, "r") as f:
     ### get key of h5 file
     # print(list(f.keys()))
@@ -16,7 +16,7 @@ with h5py.File(filename, "r") as f:
     dset = f['raw']
     pCNGRTheta, N, delay= fir_bandpass(dset[:,5], 81920., 2., 10.)
     fig, ax = plt.subplots(figsize=(100,15))
-    ax.plot(time, dset, label = "pCNGR-raw")
+    ax.plot(time, dset[:, 5], label = "pCNGR-raw")
     ax.plot(time[N-1:]-delay, pCNGRTheta[N-1:], label = "pCNGR-Filter")
     ax.legend()
-    plt.savefig('/mnt/c/Users/Desktop/0306A.png')
+    plt.show()
