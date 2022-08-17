02148db556ab142462ed5268c8558d7db7891a35
Yile Wang <yile.wang@utdallas.edu>
2022-08-17 13:11:58 -0500

    update tools

--- tools/statools.py
+++ tools/statools.py
@@ -9,7 +9,7 @@ import itertools
 import pandas as pd
 from scipy.stats import ranksums
 from scipy.stats import mannwhitneyu
-
+import scipy
 """
 This is a python script with statistical tools
 Author: Yile Wang
@@ -284,6 +284,62 @@ def bootstrap_groups(datatable, iternation:int, col:str):
         plt.legend()
     plt.show()
 
+
+def contrast_analysis(datatable, contrast, group_variable = "group", col = "gamma"):
+    """ 
+    Arg: 
+        Pandas DataFrame with all the features and groups info
+    Return: 
+        The contrast analysis results
+    
+    For this dataset, it should contain four groups, SNC, NC, MCI, AD;
+
+
+    """
+
+    # the number of cases for each group
+    num_group = len(contrast)
+    num_cases = datatable.groupby([group_variable], sort=False).count().iloc[:,0].to_numpy()
+
+    F_table = pd.DataFrame(columns=['features','F_value', 'P_value'])
+    mean_array = np.zeros(num_group)
+    var_array = np.zeros(num_group)
+
+
+    # mean calculation
+    mean_array = datatable.groupby([group_variable], sort=False).mean().loc[:,col].to_numpy()
+    meanNcontrast = np.dot(mean_array, contrast)
+    contrast2 = np.square(contrast)
+
+    # variance calculation
+    var_array = datatable.groupby([group_variable], sort=False).var().loc[:,col].to_numpy()
+    denominator = sum(num_cases) - num_group
+    # degree of freedom of the each case
+    num_cases_df = num_cases -1
+
+    # compute the sum of squares & mean sum of squares 
+    SSE = np.dot(var_array, num_cases_df)
+    MSE = SSE/denominator
+    tmp_ms_contrast = sum(contrast2/num_cases)
+
+    # compute the MS contrast
+    MS_contrast = (meanNcontrast**2) / tmp_ms_contrast
+    F_value = MS_contrast/MSE
+
+    # alpha = 0.05
+    F_critical = scipy.stats.f.ppf(q=1-0.05, dfn=1, dfd=denominator)
+
+    # for posterior contrast, using scheffe test
+    scheffe = F_critical * (num_group-1)
+    if F_value >= scheffe:
+        p = 0.05
+    else:
+        p = 'NA'
+
+    print(f"The {col} contrast has F_value {F_value}, and the F_critical Scheffe's Test is {scheffe}")
+    _tmp_dict = pd.DataFrame.from_dict([{'features':col,'F_value':F_value, 'P_value':p}])
+    F_table = pd.concat([F_table, _tmp_dict], ignore_index=True)
+    return F_table
 
 
 #############################
