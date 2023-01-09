487af7bfcc1c1ce5bd4fdcec0ba6f23c07504094
Yile Wang <yile.wang@utdallas.edu>
2023-01-09 13:21:42 -0600

    generate new lab report

--- tools/concat.py
+++ tools/concat.py
@@ -16,7 +16,7 @@ def LI_cal(file, keys, names=None):
     table = pd.read_excel(file)
     colnames = table.columns.values.tolist()
     for i in keys:
-        tmp_LI = (table.iloc[:,i] - table.iloc[:,i-1]) / (table.iloc[:,i] + table.iloc[:,i-1])
+        tmp_LI = np.abs(table.iloc[:,i] - table.iloc[:,i-1]) / (table.iloc[:,i] + table.iloc[:,i-1])
         table=pd.concat([table, tmp_LI], axis=1)
     if names == None:
         names = [*range(len(keys))]
@@ -29,7 +29,7 @@ def LI_cal(file, keys, names=None):
 
 # example:
 
-filename = "/home/yat-lok/workspace/data4project/gc3mins/ampres.xlsx"
-keys = [3,5]
-table = LI_cal(filename, keys, ('LI_amp_gamma', 'LI_amp_theta'))
-print(table)
+# filename = "/home/yat-lok/workspace/data4project/gc3mins/ampres.xlsx"
+# keys = [3,5]
+# table = LI_cal(filename, keys, ('LI_amp_gamma', 'LI_amp_theta'))
+# print(table)
