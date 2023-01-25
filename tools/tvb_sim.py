a09580931349770f4ec5cce999f63bf92e69ee58
Yat-Lok <yat-lok@labmini.local>
2023-01-25 13:05:08 -0800

    add some python test for new mac mini

--- tools/tvb_sim.py
+++ tools/tvb_sim.py
@@ -2,19 +2,19 @@ from tvb.simulator.models.stefanescu_jirsa import ReducedSetHindmarshRose
 from tvb.simulator.lab import *
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 
-def tvb_simulation(caseid, grp, gc):
+def tvb_simulation(file, gc):
 
     # file path of tvb connectome zip file
-    file = 'C:/Users/Wayne/tvb/zip/'+grp+'/'+caseid+'.zip'
     # conduction velocity
     connectivity.speed = np.array([10.])
     # simulation parameters
     sim = simulator.Simulator(
     model=ReducedSetHindmarshRose(),
-    connectivity=connectivity.Connectivity.from_file(file),
+    connectivity=connectivity.Connectivity.from_file(),
     coupling=coupling.Linear(a=np.array([gc])),
     simulation_length=1e4,
     integrator=integrators.HeunStochastic(dt=0.01220703125, noise=noise.Additive(nsig=np.array([0.00001]), ntau=0.0,
@@ -31,7 +31,13 @@ def tvb_simulation(caseid, grp, gc):
     (tavg_time, tavg_data), (raw_time, raw_data),_ = sim.run()
 
     # write it into the csv file
-    df = pd.DataFrame(raw_data[:, 0, :, 0], columns = ['aCNG-L', 'aCNG-R','mCNG-L','mCNG-R','pCNG-L','pCNG-R', 'HIP-L','HIP-R','PHG-L','PHG-R','AMY-L','AMY-R', 'sTEMp-L','sTEMP-R','mTEMp-L','mTEMp-R'])
+    # df = pd.DataFrame(raw_data[:, 0, :, 0], columns = ['aCNG-L', 'aCNG-R','mCNG-L','mCNG-R','pCNG-L','pCNG-R', 'HIP-L','HIP-R','PHG-L','PHG-R','AMY-L','AMY-R', 'sTEMp-L','sTEMP-R','mTEMp-L','mTEMp-R'])
+    df = pd.DataFrame(raw_data[:, 0, :, 0])
+    
+    return df
 
-    save_path='C:/Users/Wayne/tvb/output/'+grp+'_'+caseid+'_'+str(gc)+'.csv'
-    df.to_csv(save_path)
+
+file = "/Users/yat-lok/Desktop/0306A.zip"
+sim = tvb_simulation(file, 0.015)
+plt.plot(sim)
+plt.show()
