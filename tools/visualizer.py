bc7decd4dc74766a5ce5131ada24824ecf850b04
Yile Wang <ylwwayne@gmail.com>
2023-02-21 23:30:00 -0600

    update the fisher z transform, and add the Go optimal analysis

--- tools/visualizer.py
+++ tools/visualizer.py
@@ -4,14 +4,11 @@ from statannotations import Annotator
 import numpy as np
 import sys
 sys.path.append('/home/yat-lok/workspaces/tvbtools')
-from tools.statools import stats_calculator
 
 
 colorcoding = ["#66CDAA","#4682B4","#AB63FA","#FFA15A"]
 order = ['SNC', 'NC', 'MCI', 'AD']
 pairs = [('SNC', 'AD'),('NC', 'AD'), ('MCI', 'AD'), ('SNC', 'MCI'), ('NC', 'MCI'), ('SNC', 'NC')]
-stats_delay = stats_calculator(datatable=G, tails='less', asterisk=False)
-
 
 ### violin plot visualization
 def violin_dot(data, x, y, stats_table, test_short_name='Permutation FDR', xlabel = 'Group', ylabel='Count', pairs=pairs, order=order):
@@ -19,12 +16,12 @@ def violin_dot(data, x, y, stats_table, test_short_name='Permutation FDR', xlabe
     sig_indices = np.where(np.array(pvalues)<0.05)[0]
     fig, ax = plt.subplots()
     sns.set_style("dark")#sns.set_theme()
-    plt.box(False) #remove box
+    # plt.box(False) #remove box
     
-    plt.tick_params(left=False, labelleft=False, bottom=False) #remove ticks
+    plt.tick_params(left=False, bottom=False) #remove ticks
     sns.violinplot(data = data, x=x, y = y, inner=None, bw=.4, palette=colorcoding, linewidth=3, alpha = 1)
     sns.stripplot(data = data, x=x, y=y, edgecolor="black", linewidth=2, palette=colorcoding, alpha = 0.7, zorder=1)
-    sns.pointplot(data=data, x = x, y=y, estimator=np.mean, color="red", ci = None, edgecolor='red', join=False, markers='s', alpha=1)
+    sns.pointplot(data=data, x = x, y=y, estimator=np.mean, color="red", ci = None, join=False, markers='s', alpha=1)
     if len(sig_indices) > 0:
         sig_pvalues = pvalues[sig_indices].tolist()
         sig_pairs = [pairs[i] for i in sig_indices]
